2739548f120863a05f8eb99e2e89b1401b2ef767
Arthur Kirabo <kyagieboy99@gmail.com>
2024-02-21 20:15:21 +0300

    removed create on relation manager for agents

--- app/Filament/Resources/AgentResource/RelationManagers/BillboardsRelationManager.php
+++ app/Filament/Resources/AgentResource/RelationManagers/BillboardsRelationManager.php
@@ -59,11 +59,11 @@ class BillboardsRelationManager extends RelationManager
                 //
             ])
             ->headerActions([
-                Tables\Actions\CreateAction::make(),
+                // Tables\Actions\CreateAction::make(),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                // Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
