83526555dac4abedf7d7947b22ec1a3f703302d5
Arthur Kirabo <kyagieboy99@gmail.com>
2024-02-22 16:00:48 +0300

    implementation one time passwords

---
+++ database/migrations/2024_02_22_123400_create_one_time_passwords_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('one_time_passwords', function (Blueprint $table) {
+            $table->id();
+            $table->integer('code')->unique();
+            $table->string('phone_number')->unique();
+            $table->enum('status', ['pending', 'used', 'expired'])->default('pending');
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('one_time_passwords');
+    }
+};
