a4ba914603701bd7be833c2c037ab843536567d5
Arthur Kirabo <kyagieboy99@gmail.com>
2024-02-27 12:22:05 +0300

    Map implementation

--- app/Models/Billboard.php
+++ app/Models/Billboard.php
@@ -29,6 +29,9 @@ class Billboard extends Model
         'status',
         'agent_id',
         'is_active',
+        'lat',
+        'lng',
+        'location',
     ];
 
     protected $hidden = [
@@ -38,6 +41,11 @@ class Billboard extends Model
 
     protected $casts = [
         'is_active' => 'boolean',
+    ];
+
+
+    protected $appends = [
+        'location',
     ];
 
     //Scope Active
@@ -64,5 +72,72 @@ class Billboard extends Model
     public function agent()
     {
         return $this->belongsTo(Agent::class);
+    }
+
+    /**
+    * Returns the 'lat' and 'lng' attributes as the computed 'location' attribute,
+    * as a standard Google Maps style Point array with 'lat' and 'lng' attributes.
+    *
+    * Used by the Filament Google Maps package.
+    *
+    * Requires the 'location' attribute be included in this model's $fillable array.
+    *
+    * @return array
+    */
+
+    public function getLocationAttribute(): array
+    {
+        return [
+            "lat" => (float)$this->lat,
+            "lng" => (float)$this->lng,
+        ];
+    }
+
+    /**
+    * Takes a Google style Point array of 'lat' and 'lng' values and assigns them to the
+    * 'lat' and 'lng' attributes on this model.
+    *
+    * Used by the Filament Google Maps package.
+    *
+    * Requires the 'location' attribute be included in this model's $fillable array.
+    *
+    * @param ?array $location
+    * @return void
+    */
+    public function setLocationAttribute(?array $location): void
+    {
+        if (is_array($location))
+        {
+            $this->attributes['lat'] = $location['lat'];
+            $this->attributes['lng'] = $location['lng'];
+            unset($this->attributes['location']);
+        }
+    }
+
+    /**
+     * Get the lat and lng attribute/field names used on this table
+     *
+     * Used by the Filament Google Maps package.
+     *
+     * @return string[]
+     */
+    public static function getLatLngAttributes(): array
+    {
+        return [
+            'lat' => 'lat',
+            'lng' => 'lng',
+        ];
+    }
+
+   /**
+    * Get the name of the computed location attribute
+    *
+    * Used by the Filament Google Maps package.
+    *
+    * @return string
+    */
+    public static function getComputedLocation(): string
+    {
+        return 'location';
     }
 }
