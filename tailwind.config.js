c471f17be0e4d193f2ee86ec844d04d3bf32a62c
Arthur Kirabo <kyagieboy99@gmail.com>
2024-03-09 18:41:00 +0300

    fix for notifications

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,10 @@
 import preset from './vendor/filament/support/tailwind.config.preset'
-import colors from 'tailwindcss/colors' 
 
 export default {
     presets: [preset],
     content: [
         './app/Filament/**/*.php',
-        './resources/views/filament/**/*.blade.php',
+        './resources/views/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
     ],
-    theme: {
-        extend: {
-            colors: { 
-                danger: colors.rose,
-                primary: colors.blue,
-                success: colors.green,
-                warning: colors.yellow,
-            }, 
-        },
-    },
 }
