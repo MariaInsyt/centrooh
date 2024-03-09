658335520a3d6fbd098570f3c3bd11a5f7fc3884
Arthur Kirabo <kyagieboy99@gmail.com>
2024-03-10 00:43:26 +0300

    checking if billboard is within district assigned to agent

--- app/Filament/Resources/BillboardResource/Pages/CreateBillboard.php
+++ app/Filament/Resources/BillboardResource/Pages/CreateBillboard.php
@@ -5,8 +5,33 @@ namespace App\Filament\Resources\BillboardResource\Pages;
 use App\Filament\Resources\BillboardResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Agent;
+use Filament\Notifications\Notification;
 
 class CreateBillboard extends CreateRecord
 {
     protected static string $resource = BillboardResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        if (array_key_exists('agent_id', $data)) {
+            $agentDistricts = Agent::find($data['agent_id'])->agentDistricts->pluck('district_id')->toArray();
+
+            if (array_key_exists('district_id', $data)) {
+                if (!in_array($data['district_id'], $agentDistricts)) {
+                    Notification::make()
+                        ->warning()
+                        ->title('Hold up!')
+                        ->body('The selected agent has not been assigned to the selected district of this billboard. Please select another agent.')
+                        ->persistent()
+                        ->send();
+
+                    $this->halt();
+                }
+            }
+        }
+
+        return static::getModel()::create($data);
+    }
 }
