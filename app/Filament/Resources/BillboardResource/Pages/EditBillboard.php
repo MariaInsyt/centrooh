c56053682e97753d88b8632dbc84af6f2084ab7d
Arthur Kirabo <kyagieboy99@gmail.com>
2024-03-09 19:00:09 +0300

    fix for edit

--- app/Filament/Resources/BillboardResource/Pages/EditBillboard.php
+++ app/Filament/Resources/BillboardResource/Pages/EditBillboard.php
@@ -5,8 +5,6 @@ namespace App\Filament\Resources\BillboardResource\Pages;
 use App\Filament\Resources\BillboardResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
-use App\Models\Agent;
-use Filament\Notifications\Notification;
 
 class EditBillboard extends EditRecord
 {
@@ -17,22 +15,5 @@ class EditBillboard extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
-    }
-
-    protected function beforeSave(): void
-    {
-        $agent = Agent::find($this->record->agent_id);
-        $agent_districts = $agent->agentDistricts->pluck('district_id')->toArray();
-        $billboard_district = $this->record->district_id;
-
-        if (!in_array($billboard_district, $agent_districts)) {
-            Notification::make()
-                ->danger()
-                ->title('Hold up!')
-                ->body('Agent has not been assigned to the district of this billboard. Please assign the agent to the district of this billboard.')
-                ->persistent()
-                ->send();
-            $this->halt();
-        }
     }
 }
