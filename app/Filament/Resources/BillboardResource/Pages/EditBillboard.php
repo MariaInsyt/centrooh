486159831acd5457361aa4ebee41fa20bd6b0e18
Arthur Kirabo <kyagieboy99@gmail.com>
2024-03-09 19:21:22 +0300

    Remove beforeSave function.

--- app/Filament/Resources/BillboardResource/Pages/EditBillboard.php
+++ app/Filament/Resources/BillboardResource/Pages/EditBillboard.php
@@ -18,21 +18,4 @@ class EditBillboard extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
-
-    protected function beforeSave(): void
-    {
-        $agent = Agent::find($this->getRecord()->agent_id);
-        $agent_districts = $agent->agentDistricts->pluck('district_id')->toArray();
-        $billboard_district = $this->record->district_id;
-
-        if (!in_array($billboard_district, $agent_districts)) {
-            Notification::make()
-                ->danger()
-                ->title('Hold up!')
-                ->body('Agent has not been assigned to the district of this billboard. Please assign the agent to the district of this billboard.')
-                ->persistent()
-                ->send();
-            $this->halt();
-        }
-    }
 }
