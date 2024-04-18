1fddfc254ea00d0a9f3d1e5d28ef459ba94a968c
Arthur Kirabo <kyagieboy99@gmail.com>
2024-04-18 13:01:16 +0300

    return index.php to default

--- public/index.php
+++ public/index.php
@@ -53,7 +53,3 @@ $response = $kernel->handle(
 )->send();
 
 $kernel->terminate($request, $response);
-
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
